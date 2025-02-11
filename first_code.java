94bad95ab3601add9f153a6b913242660d48bd7c
Ajay Vishwakarma <ajayvishwakarma9711@gmail.com>
2025-02-11 22:31:06 +0530

    Data types

--- first_code.java
+++ first_code.java
@@ -1,6 +1,30 @@
 public class first_code {
-    public static void main(String[] args) {
-        System.out.println("Hello Ajay!");
+    public static void main(String a[]) {
+        // System.out.println("Hello Ajay!");
+
+        // int num1 = 2;       // introducing variables!!!
+        // int num2 = 5;
+        // int sum = num1 + num2;
+        // System.out.println(sum);
+
+        // Data types
+
+        int num = 5;
+        long l = 23343;
+        short s = 234;
+        byte b = 23;
+        
+        float f = 2.5f;
+        double d = 4.54;
+
+        char c = 'a';
+
+        boolean bo = true;
+
+
+
+
     }
     
 }
+
