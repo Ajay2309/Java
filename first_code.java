d00072ccd3365514381a386d0dc8bfc11a150806
Ajay Vishwakarma <ajayvishwakarma9711@gmail.com>
2025-02-11 22:47:11 +0530

    type_conversions

--- first_code.java
+++ first_code.java
@@ -7,19 +7,35 @@ public class first_code {
         // int sum = num1 + num2;
         // System.out.println(sum);
 
-        // Data types
+        // // Data types
 
-        int num = 5;
-        long l = 23343;
-        short s = 234;
-        byte b = 23;
+        // int num = 5;
+        // long l = 23343;
+        // short s = 234;
+        // byte b = 23;
         
-        float f = 2.5f;
-        double d = 4.54;
+        // float f = 2.5f;
+        // double d = 4.54;
 
-        char c = 'a';
+        // char c = 'a';
 
-        boolean bo = true;
+        // boolean bo = true;
+
+        //  Type Conversions
+
+        int num = 23;
+        byte k = (byte)num;
+
+        float f = 24.54f;
+        int i = (int)f;
+
+        // Type Promotion
+        byte a1 = 10;
+        byte b1 = 30;
+        int result = a1 * b1;
+        System.out.println(result);
+
+
 
 
 
