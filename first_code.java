cc2c0ce9973555df2f494fa78cd8b12d837475a5
Ajay Vishwakarma <ajayvishwakarma9711@gmail.com>
2025-02-12 22:24:36 +0530

    Arithmatic Operations

--- first_code.java
+++ first_code.java
@@ -2,12 +2,13 @@ public class first_code {
     public static void main(String a[]) {
         // System.out.println("Hello Ajay!");
 
-        // int num1 = 2;       // introducing variables!!!
+        // ***************** introducing variables!!!***********************
+        // int num1 = 2;       
         // int num2 = 5;
         // int sum = num1 + num2;
         // System.out.println(sum);
 
-        // // Data types
+        // *********************** Data types*****************************
 
         // int num = 5;
         // long l = 23343;
@@ -21,19 +22,34 @@ public class first_code {
 
         // boolean bo = true;
 
-        //  Type Conversions
+        // ************************Type Conversions************************
 
-        int num = 23;
-        byte k = (byte)num;
+        // int num = 23;
+        // byte k = (byte)num;
 
-        float f = 24.54f;
-        int i = (int)f;
+        // float f = 24.54f;
+        // int i = (int)f;
 
-        // Type Promotion
-        byte a1 = 10;
-        byte b1 = 30;
-        int result = a1 * b1;
-        System.out.println(result);
+        // **************************Type Promotion**************************
+        // byte a1 = 10;
+        // byte b1 = 30;
+        // int result = a1 * b1;
+        // System.out.println(result);
+
+        // ***********************Arithmatic Operations**********************
+        int a2 = 10;
+        int b2 = 20;
+        int sum1 = a2 + b2 ;
+        int sum2 = a2 - b2 ;
+        int sum3 = a2 * b2 ;
+        int sum4 = a2 / b2 ;
+        int sum5 = a2 % b2 ;
+
+        System.out.println(sum1);
+        System.out.println(sum2);
+        System.out.println(sum3);
+        System.out.println(sum4);
+        System.out.println(sum5);
 
 
 
