8e6dec68c819fe488759d74a85250a423622ccd1
Ajay Vishwakarma <ajayvishwakarma9711@gmail.com>
2025-02-15 22:46:54 +0530

    data_types

---
+++ data_types.java
@@ -0,0 +1,16 @@
+public class data_types {
+    public static void main(String[] args) {
+        int num = 5;
+        long l = 23343;
+        short s = 234;
+        byte b = 23;
+        
+        float f = 2.5f;
+        double d = 4.54;
+
+        char c = 'a';
+
+        boolean bo = true;
+    }
+    
+}
