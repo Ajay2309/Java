96c5d10aea55bd5c00d424b4e7ccb50b7a330f76
Ajay Vishwakarma <ajayvishwakarma9711@gmail.com>
2025-02-15 22:38:38 +0530

    first_code

--- first_code.java
+++ first_code.java
@@ -1,61 +1,6 @@
 public class first_code {
     public static void main(String a[]) {
-        // System.out.println("Hello Ajay!");
-
-        // ***************** introducing variables!!!***********************
-        // int num1 = 2;       
-        // int num2 = 5;
-        // int sum = num1 + num2;
-        // System.out.println(sum);
-
-        // *********************** Data types*****************************
-
-        // int num = 5;
-        // long l = 23343;
-        // short s = 234;
-        // byte b = 23;
-        
-        // float f = 2.5f;
-        // double d = 4.54;
-
-        // char c = 'a';
-
-        // boolean bo = true;
-
-        // ************************Type Conversions************************
-
-        // int num = 23;
-        // byte k = (byte)num;
-
-        // float f = 24.54f;
-        // int i = (int)f;
-
-        // **************************Type Promotion**************************
-        // byte a1 = 10;
-        // byte b1 = 30;
-        // int result = a1 * b1;
-        // System.out.println(result);
-
-        // ***********************Arithmatic Operations**********************
-        int a2 = 10;
-        int b2 = 20;
-        int sum1 = a2 + b2 ;
-        int sum2 = a2 - b2 ;
-        int sum3 = a2 * b2 ;
-        int sum4 = a2 / b2 ;
-        int sum5 = a2 % b2 ;
-
-        System.out.println(sum1);
-        System.out.println(sum2);
-        System.out.println(sum3);
-        System.out.println(sum4);
-        System.out.println(sum5);
-
-
-
-
-
-
+        System.out.println("Hello Ajay!");
     }
     
 }
