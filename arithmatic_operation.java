b9d047328d9c43f34e2fec4e05dbafa79f03b546
Ajay Vishwakarma <ajayvishwakarma9711@gmail.com>
2025-02-15 23:05:47 +0530

    arithmatic_operations

---
+++ arithmatic_operation.java
@@ -0,0 +1,18 @@
+public class arithmatic_operation {
+    public static void main(String[] args) {
+        int a2 = 10;
+        int b2 = 20;
+        int sum1 = a2 + b2 ;
+        int sum2 = a2 - b2 ;
+        int sum3 = a2 * b2 ;
+        int sum4 = a2 / b2 ;
+        int sum5 = a2 % b2 ;
+
+        System.out.println(sum1);
+        System.out.println(sum2);
+        System.out.println(sum3);
+        System.out.println(sum4);
+        System.out.println(sum5);
+    }
+    
+}
