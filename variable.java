11e66337b69cce279bf6c15451d8cf7643c834c2
Ajay Vishwakarma <ajayvishwakarma9711@gmail.com>
2025-02-15 22:40:11 +0530

    variables

---
+++ variable.java
@@ -0,0 +1,16 @@
+public class variable {
+    public static void main(String a[] ) {
+
+        System.out.println(2+4);
+        System.out.println(45+65);
+
+        int num1 = 2;
+        int num2 = 5;
+        System.out.println(num1+num2);
+
+        int sum = num1+num2;
+        System.out.println(sum);
+        
+    }
+    
+}
