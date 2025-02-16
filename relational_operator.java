83c96f10d22e4ca3a53cb696b50b6f2a88266d7b
Ajay Vishwakarma <ajayvishwakarma9711@gmail.com>
2025-02-16 22:50:00 +0530

    relational_operator

---
+++ relational_operator.java
@@ -0,0 +1,19 @@
+public class relational_operator {
+    public static void main(String[] args) {
+        int a = 12;
+        int b = 10;
+        
+        boolean result = a<b;
+        boolean result1 = a>b;
+        
+        System.out.println(result);
+        System.out.println(result1);
+
+        System.out.println(a<=b);
+        System.out.println(a>=b);
+        System.out.println(a==b);
+        System.out.println(a!=b);
+
+    }
+    
+}
