5dfdd28a3a030cb905cc872736a947b0a9101a77
Ajay Vishwakarma <ajayvishwakarma9711@gmail.com>
2025-02-25 22:50:07 +0530

    if_else

---
+++ if_else.java
@@ -0,0 +1,12 @@
+public class if_else {
+    public static void main(String[] args) {
+        		int x=8;
+        		System.out.println("Hello");
+        		System.out.println("Bye");
+                
+        			
+            }
+        
+        }
+    
+}
