2ea46c9fea8037598eec45a261ad0de9ab24e4f9
Ajay Vishwakarma <ajayvishwakarma9711@gmail.com>
2025-02-25 22:56:02 +0530

    if else

--- if_else.java
+++ if_else.java
@@ -19,14 +19,21 @@ public class if_else {
         		// }
         		// System.out.println("Bye");
                 
-        		int x=28;
-        		if(x>10 && x<=20) {       //11-20
-        			System.out.println("Hello");
-        		}
-        		else
-        		System.out.println("Bye");
+        		// int x=28;
+        		// if(x>10 && x<=20) {       //11-20
+        		// 	System.out.println("Hello");
+        		// }
+        		// else
+        		// System.out.println("Bye");
                 
-                
+                int x=8;
+                int y=7;
+                if(x>y) {
+                    System.out.println(x);
+                    System.out.println("Thankyou");
+                }
+                else
+                    System.out.println(y);	
         			
             
         
