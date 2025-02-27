278c525f617b43d39b5dc16becb5b419279973ab
Ajay Vishwakarma <ajayvishwakarma9711@gmail.com>
2025-02-27 22:19:23 +0530

    if else if

---
+++ if_else_if.java
@@ -0,0 +1,19 @@
+public class if_else_if {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 7;
+        int c = 9;
+
+        if (a>b && a>c)
+            System.out.println(a);
+        
+        // else if (b>a && b>c)   
+
+        else if (b>c)     // we always try to remove the unnecessary code.
+            System.out.println(b);
+
+        else 
+            System.out.println(c);
+    }
+    
+}
