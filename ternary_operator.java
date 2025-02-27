e8064cb9c07d27ae3a0f1e385f35853f59ec6c06
Ajay Vishwakarma <ajayvishwakarma9711@gmail.com>
2025-02-27 22:30:24 +0530

    ternary operator

---
+++ ternary_operator.java
@@ -0,0 +1,23 @@
+public class ternary_operator {
+    public static void main(String [] args) {
+
+        int a = 4;
+        int result = 0;
+
+        // if (a%2==0)
+        //     result = 10;
+        // else
+        //     result = 20;
+
+        // converting upper stuf in ternary code......
+
+        result = a%2==0 ? 10 : 20 ;
+
+        System.out.println(result);
+
+// Syntax:
+// variable = (condition)?value1 if condition is true :value2 if condition is false
+
+    }
+    
+}
