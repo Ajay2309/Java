6edac76822d22ca6943ef80de0b0db08f9eb7869
Ajay Vishwakarma <ajayvishwakarma9711@gmail.com>
2025-03-02 20:51:41 +0530

    for loop

---
+++ for_loop.java
@@ -0,0 +1,32 @@
+public class for_loop {
+    public static void main(String a[]){
+        
+        for(int i=1 ; i<=4 ; i++)
+        {
+            System.out.println("Hello " + i);
+        }
+
+
+
+        // for(int i = 1 ; i <= 5 ; i++)    // nested loop
+        // {
+        //     System.out.println("Day "+i);
+        //         for(int j=1; j<=8 ; j++)
+        //         {
+        //             System.out.println("  "+ (j+8)+ " - " + (j+9));
+        //         }
+
+        // }
+
+
+        // another formate in whcih we can write it......
+
+        // int i = 1;
+        // for(;i<=5;)
+        // {
+        //     System.out.println("Hi "+ i);
+        //     i++;
+        // }
+    }
+    
+}
