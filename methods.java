078f4c14e6608f63cf8c1cac8a5ea4ad8e36c954
Ajay Vishwakarma <ajayvishwakarma9711@gmail.com>
2025-03-06 22:51:50 +0530

    methods

--- methods.java
+++ methods.java
@@ -1,6 +1,18 @@
 public class methods {
     public static void main(String[] args) {
-        System.out.println("hello");
+        class Computer
+        {
+            public void playMusic()
+            {
+                System.out.println("Music Playing...");
+            }
+            public String getMeAPen(int cost)
+            {
+                if(cost>=10)
+                    return "Pen";
+                else
+                    return "Nothing";
+            }
+        }
     }
-    
 }
