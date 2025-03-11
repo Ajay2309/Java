9ee754b8f96c890855558a7ccf7b424c72d12948
Ajay Vishwakarma <ajayvishwakarma9711@gmail.com>
2025-03-11 22:20:26 +0530

    Array

---
+++ Array.java
@@ -0,0 +1,25 @@
+public class Array {
+    public static void main(String a[]){
+
+        // int num[] = {2,4,6,8};
+        // num[1] = 23;
+        // System.out.println(num[1]);
+
+        int num[] = new int[4];
+        num[0]= 7;
+        num[1]= 5;
+        num[2]= 4;
+        num[3]= 9;
+
+        // System.out.println(num[0]);
+        // System.out.println(num[1]);
+        // System.out.println(num[2]);
+        // System.out.println(num[3]);
+
+        for(int i=0; i<4; i++)
+        {
+            System.out.println(num[i]);
+        }
+    }
+    
+}
