fbb13ed97752504a32c51e50b23a4510159129a0
Ajay Vishwakarma <ajayvishwakarma9711@gmail.com>
2025-03-11 18:01:24 +0530

    methods

--- methods.java
+++ methods.java
@@ -1,18 +1,24 @@
+class computer
+{
+    public void playMusic()
+    {
+        System.out.println("Playing Music.....");
+    }
+
+    public String getMePen(int cost)
+    {
+        return "Pen";
+    }
+}
+
 public class methods {
-    public static void main(String[] args) {
-        class Computer
-        {
-            public void playMusic()
-            {
-                System.out.println("Music Playing...");
-            }
-            public String getMeAPen(int cost)
-            {
-                if(cost>=10)
-                    return "Pen";
-                else
-                    return "Nothing";
-            }
-        }
+    public static void main(String a[]){
+        computer obj = new computer();
+        obj.playMusic();
+        // obj.getMePen(10);   // this wiss not work
+
+        String str = obj.getMePen(2);
+        System.out.println(str);
+
     }
 }
