97f3cfd4173efc3d6e117ff15ddd275f48826ca3
Ajay Vishwakarma <ajayvishwakarma9711@gmail.com>
2025-03-15 23:24:15 +0530

    multi dimensional array

--- multi_dimensional_array.java
+++ multi_dimensional_array.java
@@ -1,5 +1,25 @@
 public class multi_dimensional_array {
     public static void main(String[] args) {
+        int nums [] [] = new int [3] [4];
+
+        // input random values
+
+        for(int i=0; i<3; i++)
+        {
+            for(int j=0; i<4; j++)
+            {
+                nums [i] [j] = (int)(Math.random() * 10);
+            }
+        }
+
+        for(int i=0; i<3; i++)
+        {
+            for(int j=0; j<4; j++)
+            {
+                System.out.print(nums [i] [j] + " ");
+            }
+            System.out.println();
+        }
         
     }
     
